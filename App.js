4263c213a9533f3dea05d1d4cb41d14bb0fec110
Michael Papesca <michael.papesca@onmail.com>
2021-01-28 20:31:58 -0500

    Use express to bind to port

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
-
-
 require('dotenv').config();
+var express = require('express');
 const Discord = require('discord.js');
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function (request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function () {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
